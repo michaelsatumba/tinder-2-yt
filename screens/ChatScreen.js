27eae99949c1140714df782a058c507e85a6c712
michaelsatumba <mjsatumba@gmail.com>
2021-11-29 15:43:42 -0800

    starting chat

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,11 +1,17 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView } from 'react-native'
+import ChatList from '../components/ChatList';
+import Header from '../components/Header';
 
 const ChatScreen = () => {
     return (
-        <View>
-            <Text>I am the ChatScreen</Text>
-        </View>
+        <SafeAreaView>
+            <Header title="Chat" 
+            // callEnabled 
+            />
+            <ChatList />
+
+        </SafeAreaView>
     );
 };
 
